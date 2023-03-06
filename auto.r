647feb12dcafbce28cc12626c64bb775252d940e
chucktheivy <charliewilson@ucsb.edu>
2023-03-05 23:17:55 -0800

    Finished Rough Draft

--- auto.r
+++ auto.r
@@ -8,7 +8,7 @@ auto = function(m,V,A,crolling = 0.015,cdrag = 0.3, p_air = 1.2, g = 9.8) {
 # Pb is power in Watts
   
   
-  pb = (crolling*m*g*V)+(.5*A*p_air*cdrag*(V^3))
+  pb = (crolling*m*g*(V*.287))+(.5*A*p_air*cdrag*(V^3))
   
   return(pb)
 }
